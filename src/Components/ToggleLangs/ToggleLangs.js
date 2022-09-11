d6e1d63c53f26b10639ae1734fe7e046068c263b
Paloma <celinipaloma@gmail.com>
2022-09-11 18:31:06 +0200

    v1

---
+++ src/Components/ToggleLangs/ToggleLangs.js
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import FrenchFlag from "../../assets/france.svg";
+import SpanishFlag from "../../assets/spain.svg";
+import EnglishFlag from "../../assets/united-kingdom.svg";
+import { Context } from "../../context/langContext";
+import './ToggleLangs.css';
+const ToggleLangs = () => {
+  const {toggleLang} = useContext(Context)
+  return (
+  <div className="container-langs">
+    <img onClick={() => toggleLang('FR')} src={FrenchFlag} alt=""/>
+    <img onClick={() => toggleLang('EN')} src={EnglishFlag} alt=""/>
+    <img onClick={() => toggleLang('ES')} src={SpanishFlag} alt=""/>
+  </div>)
+};
+
+export default ToggleLangs;
